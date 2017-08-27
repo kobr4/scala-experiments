f5db8818afaf7d6b41d32242a531a66470b589f2
Nicolas My <nicolas.my@gmail.com>
2017-08-27 23:36:05 +0200

    Added Slack connectivity

--- akka-experiment-02/build.sbt
+++ akka-experiment-02/build.sbt
@@ -1,5 +1,6 @@
 scalaVersion := "2.12.3"
 
-libraryDependencies += "com.typesafe.play" %% "play-json" % "2.6.3"
+libraryDependencies += "com.typesafe.play" %% "play-json" % "2.6.0-M1"
 libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.0.9"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+libraryDependencies += "com.github.gilbertw1" %% "slack-scala-client" % "0.2.1"
--- akka-experiment-02/src/main/scala/com/kobr4/snippets/WebServer.scala
+++ akka-experiment-02/src/main/scala/com/kobr4/snippets/WebServer.scala
@@ -4,11 +4,12 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
+import com.kobr4.polo.PairPrice
 
 import scala.concurrent.ExecutionContextExecutor
-import scala.io.StdIn
-
+import scala.io.StdIn // Async
 
 object WebServer {
   def main(args: Array[String]) {
@@ -18,13 +19,29 @@ object WebServer {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
+    SlackBot.start(args(0))
+
     val route =
       path("hello") {
         get {
           complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
         }
-      }
+      } ~
+        path("quote" / Segment) { pairName =>
+          get {
+            onSuccess(Http().singleRequest(HttpRequest(uri = "https://poloniex.com/public?command=returnTicker"))) {
+              response =>
+                onSuccess(Unmarshal(response.entity).to[String]) { body =>
+                  val httpResp = PairPrice.fromString(body).filter(pair => pair.name.contains(pairName)).map {
+                    pair =>
+                      HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>${pair.name}:${pair.last}</h1>")
+                  }.headOption.getOrElse(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Pair not found</h1>"))
 
+                  complete(httpResp)
+                }
+            }
+          }
+        }
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
