5faed319ad71d6553c1cd5cdd2928f092c9cc613
Kobr4 <nicolas.my@gmail.com>
2018-04-06 00:40:36 +0200

    Added order processing

--- trading-bot/src/main/scala/com/kobr4/tradebot/Order.scala
+++ trading-bot/src/main/scala/com/kobr4/tradebot/Order.scala
@@ -7,3 +7,21 @@ case class Buy(asset: Asset, price: BigDecimal, quantity: BigDecimal) extends Or
 case class Sell(asset: Asset, price: BigDecimal, quantity: BigDecimal) extends Order
 
 case class Quantity(quantity: BigDecimal)
+
+object Order {
+
+  private def getCurrencyPair(asset: Asset): String = {
+    asset match {
+      case Asset.Btc => "BTC_USD"
+      case Asset.Eth => "ETH_USD"
+    }
+  }
+
+  def process(order: Order): Unit = {
+    val api = new PoloApi()
+    order match {
+      case b: Buy => api.buy(getCurrencyPair(b.asset), b.price, b.quantity)
+      case s: Sell => api.sell(getCurrencyPair(s.asset), s.price, s.quantity)
+    }
+  }
+}
