9725e31ffe01e4d71c71abc6d4a100de670682ed
Kobr4 <nicolas.my@gmail.com>
2018-04-10 00:07:16 +0200

    trading ops

---
+++ trading-bot/src/main/scala/com/kobr4/tradebot/TradingOps.scala
@@ -0,0 +1,35 @@
+package com.kobr4.tradebot
+
+import scala.concurrent.ExecutionContext
+
+class TradingOps(val api: PoloApi)(implicit ec: ExecutionContext) {
+
+  private def getAmount(currencyPair: CurrencyPair, asset: Asset, rate: BigDecimal, quantity: BigDecimal): BigDecimal = {
+    if (currencyPair.left == asset)
+      quantity / rate
+    else
+      quantity * rate
+  }
+
+  def buyAtMarketValue(currencyPair: CurrencyPair, asset: Asset, quantity: Quantity): Unit = {
+    api.returnTicker().map { quoteList =>
+      quoteList.filter(q => (q.pair.left == currencyPair.left)
+        && (q.pair.right == currencyPair.right)).foreach { q =>
+        api.buy(currencyPair.toString, q.last, getAmount(currencyPair, asset, q.last, quantity.quantity))
+      }
+    }
+  }
+
+  def sellAtMarketValue(currencyPair: CurrencyPair, asset: Asset, quantity: Quantity): Unit = {
+    api.returnTicker().map { quoteList =>
+      quoteList.filter(q => (q.pair.left == currencyPair.left)
+        && (q.pair.right == currencyPair.right)).foreach { q =>
+        api.sell(currencyPair.toString, q.last, getAmount(currencyPair, asset, q.last, quantity.quantity))
+      }
+    }
+  }
+
+  def cancelAllOpenOrders(): Unit = {
+    api.returnOpenOrders().map { orderList => orderList.map { order => api.cancelOrder(order.orderNumber) }}
+  }
+}
