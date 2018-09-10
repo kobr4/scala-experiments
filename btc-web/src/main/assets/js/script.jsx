4c57180cb1eba2973f61486923d9c5ab7d7d180b
Kobr4 <nicolas.my@gmail.com>
2018-09-10 11:20:18 +0200

    More react code

--- btc-web/src/main/assets/js/script.jsx
+++ btc-web/src/main/assets/js/script.jsx
@@ -1,24 +1,52 @@
 'use strict';
 
 function ApiResponseField(props) {
-  return <tr><td>{props.name}</td><td>{props.field}</td></tr>;
+  return <tr><td>{props.name}</td><td>{props.value}</td></tr>;
 }
 
 class ApiResponse extends React.Component {
 
   constructor(props) {
     super(props);
-
     this.state = { responseFields : [] }
+  }
+
+  updateState(fields) {
+    this.setState( { responseFields : fields });
+  }
+
+  updateTimed(target) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             var jsonResponse = JSON.parse(this.responseText)
+             var fields = [];
+             Object.keys(jsonResponse.result).forEach(function (key) {
+               var value = JSON.stringify(jsonResponse.result[key]);
+                var field = <ApiResponseField name={key} value={value} key={key} />
+                fields.push(field);
+             });
+             target.updateState(fields);
+         }
+    };
+    xhttp.open("GET", "http://localhost:8080/btc-api/getblockchaininfo", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.updateTimed(this), 5000);
   }
 
   render() {
     return (
        <table>
+       <tbody>
        <tr><th>Name</th><th>Value</th></tr>
        {
          this.state.responseFields.map(field => field)
        }
+       </tbody>
        </table>
     );
   }
@@ -28,4 +56,6 @@ ReactDOM.render(
   <ApiResponse />,
   document.getElementById('toto')
 );
+
+
 
