c4503c9c05791034e73fa1bb9fde2ac5f2c9f2e4
Kobr4 <nicolas.my@gmail.com>
2018-09-16 16:22:50 +0200

    Change config to answer on all interfaces

--- btc-web/src/main/scala/com/nicolasmy/QuickstartServer.scala
+++ btc-web/src/main/scala/com/nicolasmy/QuickstartServer.scala
@@ -29,9 +29,9 @@ object QuickstartServer extends App with UserRoutes with BtcRoutes {
   //#main-class
 
   //#http-server
-  Http().bindAndHandle(routes, "localhost", 8080)
+  Http().bindAndHandle(routes, "0.0.0.0", 8080)
 
-  println(s"Server online at http://localhost:8080/")
+  println(s"Server online at http://0.0.0.0:8080/")
 
   Await.result(system.whenTerminated, Duration.Inf)
   //#http-server
